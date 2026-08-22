cd1c61042da99e2b54c71f61412cbbdb203b4ecb
Kxi723 <guanxi723@gmail.com>
2026-08-23 01:25:46 +0800

    add yungcee

--- detectors/__init__.py
+++ detectors/__init__.py
@@ -16,10 +16,13 @@ class DefectSpec:
 
 # The order shown in the menu.
 DEFECTS: List[DefectSpec] = [
-    DefectSpec("jason_damage_by_fold", "Damage by Fold - Jason Lai"),
+    DefectSpec("jason_damage_by_fold", "Damage by Fold - Jason Lai Kwang Xi"),
     DefectSpec("jason_dirty", "Dirty - Jason Lai"),
-    DefectSpec("jason_tearing_at_finger", "Tearing(fingertip) - Jason Lai"),
     DefectSpec("sheng_finger_not_enough", "Finger Not Enough - Sheng"),
-    DefectSpec("sheng_plastic_contamination", "Plastic Contamination - Sheng"),
     DefectSpec("sheng_improper_roll", "Improper Roll - Sheng"),
+    DefectSpec("oversized", "Oversize - Yung Cee"),
+    DefectSpec("sheng_plastic_contamination", "Plastic Contamination - Sheng"),
+    DefectSpec("stain", "Stain - Yung Cee"),
+    DefectSpec("jason_tearing_at_finger", "Tearing(fingertip) - Jason Lai Kwang Xi"),
+    DefectSpec("wrinkle", "Wrinkle - Yung Cee"),
 ]
